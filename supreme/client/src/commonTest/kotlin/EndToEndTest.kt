9c54f97971fd2adaa9fe23bb8101fc1333c7c9db
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-11-15 23:15:43 +0100

    fix tests

--- supreme/client/src/commonTest/kotlin/EndToEndTest.kt
+++ supreme/client/src/commonTest/kotlin/EndToEndTest.kt
@@ -1,5 +1,7 @@
 import at.asitplus.attestation.supreme.*
+import at.asitplus.signum.indispensable.pki.leaf
 import at.asitplus.signum.supreme.os.PlatformSigningProvider
+import at.asitplus.test.Target
 import de.infix.testBalloon.framework.core.testSuite
 import io.kotest.assertions.withClue
 import io.kotest.matchers.shouldBe
@@ -14,28 +16,41 @@ val ENDPOINT_SHUTDOWN = "http://10.0.2.2:8080/shutdown"
 val ALIAS = "ALIAS"
 
 val EndToEndTest by testSuite {
+    //This test lives here due to IDEA not recognizing androidDevicTest sources being wired to commonTest
+    //to not make ios fail, we guard it here
+    if (Target.current == Target.ANDROID_ART) {
+
+        test("endToEnd") {
+            PlatformSigningProvider.deleteSigningKey(ALIAS)
+            val client = AttestationClient(HttpClient())
+            val resp = client.getChallenge(Url(ENDPOINT_CHALLENGE))
+            withClue("Fetch Challenge") {
+                resp.isSuccess shouldBe true
+            }
+            val attestationChallenge: AttestationChallenge = resp.getOrThrow()
+            val csr = withClue("Create attested key and CSR") {
+                attestationChallenge.createAttestationProof(ALIAS).getOrThrow()
+            }
+
+            val result = client.attest(csr, attestationChallenge.attestationEndpointUrl)
+            val clue =
+                if (result is AttestationResponse.Failure)
+                    result.kind.name + ": " + (result.explanation ?: "FAIL")
+                else ""
+            withClue(clue) {
+                result.shouldBeInstanceOf<AttestationResponse.Success>()
+                withClue("Cert leaf pub key is the original attested key") {
+                    result.certificateChain.leaf.decodedPublicKey.getOrThrow() shouldBe PlatformSigningProvider.getSignerForKey(
+                        ALIAS
+                    ).getOrThrow().publicKey
+                }
+            }
+
+        }
 
 
-    test("endToEnd") {
-        PlatformSigningProvider.deleteSigningKey(ALIAS)
-        val client = AttestationClient(HttpClient())
-        val resp = client.getChallenge(Url(ENDPOINT_CHALLENGE))
-        resp.isSuccess shouldBe true
-        val attestationChallenge: AttestationChallenge = resp.getOrThrow()
-
-        val csr = attestationChallenge.createAttestationProof(ALIAS).getOrThrow()
-        val result = client.attest(csr, attestationChallenge.attestationEndpointUrl)
-        val clue =
-            if (result is AttestationResponse.Failure)
-                result.kind.name + ": " + (result.explanation ?: "FAIL")
-            else ""
-        withClue(clue) { result.shouldBeInstanceOf<AttestationResponse.Success>() }
-
+        test("shutdown") {
+            HttpClient().get(ENDPOINT_SHUTDOWN)
+        }
     }
-
-
-    test("shutdown") {
-        HttpClient().get(ENDPOINT_SHUTDOWN)
-    }
-
 }
