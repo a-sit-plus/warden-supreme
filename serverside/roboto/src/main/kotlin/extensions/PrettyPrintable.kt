a2c5d52e23c8355264c5e3575ffc26b69e7f4421
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-01-26 10:04:08 +0100

    move custom parser to commons

--- serverside/roboto/src/main/kotlin/extensions/PrettyPrintable.kt
+++
@@ -1,7 +0,0 @@
-package at.asitplus.attestation.android
-
-interface PrettyPrintable {
-    fun doPrettyPrint(indent: String): String
-}
-
-fun PrettyPrintable.prettyPrint(): String = doPrettyPrint("  ").trimIndent()
