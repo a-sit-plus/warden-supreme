82879aa896ff2978a7dc211c9cb8e8a8acdaf010
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-02-11 11:33:36 +0100

    Feature/engine check (#61)

--- supreme/common/src/jvmTest/kotlin/DebugStatementParserTest.kt
+++ supreme/common/src/jvmTest/kotlin/DebugStatementParserTest.kt
@@ -1,7 +1,7 @@
-import at.asitplus.attestation.android.AttestationKeyDescription
-import at.asitplus.attestation.android.androidAttestationExtension
+import at.asitplus.attestation.android.*
 import at.asitplus.catchingUnwrapped
 import at.asitplus.io.MultiBase
+import at.asitplus.signum.indispensable.io.Base64UrlStrict
 import at.asitplus.signum.indispensable.toKmpCertificate
 import at.asitplus.testballoon.invoke
 import at.asitplus.testballoon.minus
@@ -9,9 +9,11 @@ import at.asitplus.testballoon.withData
 import com.android.keyattestation.verifier.KeyDescription
 import com.google.android.attestation.ParsedAttestationRecord
 import de.infix.testBalloon.framework.core.testSuite
+import io.kotest.assertions.withClue
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
@@ -21,24 +23,59 @@ import org.bouncycastle.asn1.DEROctetString
 import java.io.ByteArrayInputStream
 import java.security.cert.X509Certificate
 import java.util.*
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
 import kotlin.io.bufferedReader
 import kotlin.io.forEachLine
+import kotlin.time.Duration
+import kotlin.time.Instant
 import kotlin.use
 
+private typealias DebugDetails = Pair<Instant, ByteArray>
+
+val DebugDetails.verificationTime get() = first
+val DebugDetails.challenge get() = second
+
+private typealias DebugStmt = Triple<List<String>, AndroidAttestationConfiguration, DebugDetails>
+
+val DebugStmt.certificateChain get() = first
+val DebugStmt.cfg get() = second
+val DebugStmt.details get() = third
+
+
+@OptIn(ExperimentalAtomicApi::class)
 val DebugStatementParserTest by testSuite {
     val json = Json { ignoreUnknownKeys = true }
     val classLoader = Thread.currentThread().contextClassLoader
 
 
-    val proofs: List<List<String>> =
+    val proofs: List<DebugStmt> =
         catchingUnwrapped {
             classLoader.getResourceAsStream("DebugStatements.csv").bufferedReader(Charsets.UTF_8).use {
 
-                val lst = mutableListOf<List<String>>()
+                val lst = mutableListOf<DebugStmt>()
                 it.forEachLine { line ->
 
                     val parsed = json.parseToJsonElement(MultiBase.decode(line)!!.decodeToString()).jsonObject
-                    lst += (parsed.getValue("genericAttestationProof").jsonArray).map { it.jsonPrimitive.content }
+                    val timestamp = Instant.parse(parsed.getValue("verificationTime").jsonPrimitive.content)
+                    val offset = Duration.parse(parsed.getValue("verificationTimeOffset").jsonPrimitive.content)
+                    val challenge =
+                        parsed.getValue("challenge").jsonPrimitive.content.decodeToByteArray(Base64UrlStrict)
+                    val androidApp =
+                        parsed.getValue("androidAttestationConfiguration").jsonObject.getValue("applications").jsonArray.first().jsonObject
+                    val pkg = androidApp.getValue("packageName").jsonPrimitive.content
+
+                    val digests = androidApp.getValue("signatureDigests").jsonArray.map {
+                        it.jsonPrimitive.content.decodeToByteArray(Base64UrlStrict)
+                    }
+
+                    val attestationCfg =
+                        AndroidAttestationConfiguration(
+                            AndroidAttestationConfiguration.AppData(pkg, digests),
+                            ignoreLeafValidity = true,
+                            verificationSecondsOffset = offset.inWholeSeconds
+                        )
+                    val chain = (parsed.getValue("genericAttestationProof").jsonArray).map { it.jsonPrimitive.content }
+                    lst += DebugStmt(chain, attestationCfg, DebugDetails(timestamp, challenge))
 
                 }
                 lst
@@ -55,9 +92,9 @@ val DebugStatementParserTest by testSuite {
         return@testSuite
     }
 
-    withData(proofs.withIndex()) - { (index, it) ->
+    withData(nameFn = { "${it.value.first.size} certs" }, proofs.withIndex()) - { (index, it) ->
         val attestationCertChain =
-            it.map {
+            it.certificateChain.map {
                 val certBytes = Base64.getUrlDecoder().decode(it)
                 catchingUnwrapped {
                     certificateFactory.generateCertificate(
@@ -89,7 +126,7 @@ val DebugStatementParserTest by testSuite {
             }
 
             if (index > 0) {
-                val prev = proofs[index - 1].map {
+                val prev = proofs[index - 1].certificateChain.map {
                     val certBytes = Base64.getUrlDecoder().decode(it)
                     catchingUnwrapped {
                         certificateFactory.generateCertificate(
@@ -142,6 +179,62 @@ val DebugStatementParserTest by testSuite {
                 assertSemanticallyEqual(fromGoogle, androidAttestationExtension)
 
 
+            }
+            "Roboto Engine Check" - {
+                val stmt = it
+                //not only is revocation slow. it will also have us run into rate limits
+                //plus, many have been revoked and we also want success results!
+                val rSupreme = stmt.cfg.copy(supremeParser = true, revocation = emptyList())
+                val rLegacy = stmt.cfg.copy(supremeParser = false, revocation = emptyList())
+
+
+                withData(
+                    listOf(
+                        rSupreme to rLegacy,
+                        rSupreme.copy(patchLevel = PatchLevel(2025, 1)) to
+                                rLegacy.copy(patchLevel = PatchLevel(2025, 1)),
+                        rSupreme.copy(patchLevel = null, androidVersion = null) to
+                                rLegacy.copy(patchLevel = null, androidVersion = null),
+                        rSupreme.copy(requireRemoteKeyProvisioning = true) to rLegacy.copy(requireRemoteKeyProvisioning = true),
+                        rSupreme.copy(ignoreLeafValidity = false) to rLegacy.copy(ignoreLeafValidity = false),
+                        rSupreme.copy(requireStrongBox = true) to rLegacy.copy(requireStrongBox = true),
+                        rSupreme.copy(enableSoftwareAttestation = true) to rLegacy.copy(enableSoftwareAttestation = true),
+                        rSupreme.copy(allowBootloaderUnlock = true) to rLegacy.copy(allowBootloaderUnlock = true),
+                        //get the "UnknownPackage" one step further
+                        rSupreme.copy(
+                            applications = listOf(
+                                AndroidAttestationConfiguration.AppData(
+                                    "UnknownPackage",
+                                    signerFingerprints = listOf(byteArrayOf())
+                                )
+                            )
+                        ) to rLegacy.copy(
+                            applications = listOf(
+                                AndroidAttestationConfiguration.AppData(
+                                    "UnknownPackage",
+                                    signerFingerprints = listOf(byteArrayOf())
+                                )
+                            )
+                        )
+                    ),
+                    compact = true
+                ) { (rSupreme, rLegacy) ->
+                    val supreme = Roboto(rSupreme).verify(
+                        attestationCertChain,
+                        stmt.details.verificationTime,
+                        stmt.details.challenge
+                    )
+                    val legacy = Roboto(rLegacy).verify(
+                        attestationCertChain,
+                        stmt.details.verificationTime,
+                        stmt.details.challenge
+                    )
+
+                    //needs toString, because types inside result differ on error
+                    withClue("Supreme success: ${supreme.isSuccess}, Legacy success: ${legacy.isSuccess}") {
+                        supreme.toString() shouldBe legacy.toString()
+                    }
+                }
             }
 
         } else "Empty chain" {}
